4f2324449fa5b9e793a6ee23381c76f610da59dd
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-09-07 21:07:02 +0900

    First commit

---
+++ src/test/java/com/kb/zipkeem/ZipkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.zipkeem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
