3bf2a8434d99e93ef2bfb7536366655874f36a32
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-09-11 13:21:08 +0900

    feat: 매물등록시 등록날짜추가 기능 구현

--- src/main/java/com/kb/zipkim/ZipkimApplication.java
+++ src/main/java/com/kb/zipkim/ZipkimApplication.java
@@ -3,8 +3,10 @@ package com.kb.zipkim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableFeignClients
 public class ZipkimApplication {
 
