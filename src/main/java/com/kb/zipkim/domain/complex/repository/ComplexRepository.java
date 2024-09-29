b5bd97defca666d4cbdfb781f5af86bb45a6c3a1
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-09-29 17:21:25 +0900

    feat: 법정동코드로 단지검색 추가

--- src/main/java/com/kb/zipkim/domain/complex/repository/ComplexRepository.java
+++ src/main/java/com/kb/zipkim/domain/complex/repository/ComplexRepository.java
@@ -3,9 +3,11 @@ package com.kb.zipkim.domain.complex.repository;
 import com.kb.zipkim.domain.complex.entity.Complex;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ComplexRepository extends JpaRepository<Complex, Long> {
 
     Optional<Complex> findByBgdCdAndMainAddressNoAndSubAddressNo(String bgdCd, String mainAddressNo, String subAddressNo);
+    List<Complex> findByBgdCd(String bgdCd);
 }
