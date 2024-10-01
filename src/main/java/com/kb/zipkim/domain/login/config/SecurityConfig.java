e0e50f636622eedc80e9eff9e1e0e282a0d42909
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-01 21:08:59 +0900

    다시 필터해제

--- src/main/java/com/kb/zipkim/domain/login/config/SecurityConfig.java
+++ src/main/java/com/kb/zipkim/domain/login/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())    //csrf disable
                 .formLogin(login -> login.disable())   //From 로그인 방식 disable
                 .httpBasic(basic -> basic.disable())   //HTTP Basic 인증 방식 disable
-                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)   // JWTFilter 추가
+//                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)   // JWTFilter 추가
                 .oauth2Login((oauth2) -> oauth2     //oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService))
@@ -71,7 +71,7 @@ public class SecurityConfig {
                        .requestMatchers("/").permitAll()
                        .anyRequest().permitAll())
 //                         .anyRequest().authenticated())
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(customAuthenticationEntryPoint))
+//                .exceptionHandling(exception -> exception.authenticationEntryPoint(customAuthenticationEntryPoint))
                 .sessionManagement((session) -> session     //세션 설정 : STATELESS
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
