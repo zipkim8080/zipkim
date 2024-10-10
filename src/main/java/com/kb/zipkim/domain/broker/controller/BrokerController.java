cef7b4498cc67afb0d55b5e486f4cfec85874a12
Kim Jong Yeong <621kkjy@gmail.com>
2024-10-10 21:03:08 +0900

    feat: 중개자격번호 조회시 role 변경

--- src/main/java/com/kb/zipkim/domain/broker/controller/BrokerController.java
+++ src/main/java/com/kb/zipkim/domain/broker/controller/BrokerController.java
@@ -2,7 +2,12 @@ package com.kb.zipkim.domain.broker.controller;
 
 import com.kb.zipkim.domain.broker.dto.BrokerDTO;
 import com.kb.zipkim.domain.broker.service.BrokerService;
+import com.kb.zipkim.domain.login.jwt.JWTUtil;
+import com.kb.zipkim.domain.login.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +19,8 @@ import java.util.Map;
 public class BrokerController {
 
     private final BrokerService brokerService;
+    private final JWTUtil jwtUtil;
+    private final UserService userService;
 
     @PostMapping("/api/broker")
     public BrokerDTO findBroker(@RequestBody Map<String, String> request) {
@@ -22,4 +29,12 @@ public class BrokerController {
 
         return brokerService.getBroker(name, brokerNumber);
     }
+
+    @GetMapping("/api/role")
+    public ResponseEntity getRole(HttpServletRequest request) {
+        String username = jwtUtil.getUsername(request.getHeader("Authorization").substring(7));
+        String role = userService.getRole(username);
+        return ResponseEntity.ok(role);
+    }
+
 }
