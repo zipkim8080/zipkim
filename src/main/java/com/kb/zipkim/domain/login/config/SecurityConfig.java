ee151004605b02dd63005ff36825168dc25a41d2
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-10-13 00:45:46 +0900

    fix: cors수정

--- src/main/java/com/kb/zipkim/domain/login/config/SecurityConfig.java
+++ src/main/java/com/kb/zipkim/domain/login/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -45,7 +46,7 @@ public class SecurityConfig {
 
                         CorsConfiguration configuration = new CorsConfiguration();
 
-                        configuration.setAllowedOrigins(Collections.singletonList("*"));
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://your-amplify-domain.com"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
